f7a98e80d7eb97a7d4752410ccff8d519bc29f41
CarlosMS <programer.cm12@gmail.com>
2026-03-12 12:46:41 -0500

    Implementacion terminada de Registrar.java y Listar con su layout

--- app/src/main/java/com/example/appmascotas/Listar.java
+++ app/src/main/java/com/example/appmascotas/Listar.java
@@ -1,6 +1,11 @@
 package com.example.appmascotas;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Listar extends AppCompatActivity {
 
+    ListView lstMascotas;
+    RequestQueue requestQueue;
+
+    //URL
+    private final String URL = "http://192.168.101.34:3000/mascotas";
+
+    private void loadUI(){
+        lstMascotas = findViewById(R.id.lstMascotas);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +43,52 @@ public class Listar extends AppCompatActivity {
         setContentView(R.layout.activity_listar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        loadUI();
+        obtenerDatos();
+    }
+    //Importar objetos JSON desde WS
+    private void obtenerDatos(){
+        requestQueue = Volley.newRequestQueue(this);
+
+        //¿Que nos devuelve el WS con el metodo GET?
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        renderizarListView(jsonArray);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.e("ErrorWS",volleyError.toString());
+                        Toast.makeText(getApplicationContext(),"No se obtuvieron los datos",Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
+    } //obtenerDatos
+
+    //Poblar (llenar) ListView con los JSON
+    private void renderizarListView(JSONArray jsonMascotas){
+        try{
+            //Conector, traductor, medio carga de datos
+            ArrayAdapter adapter;
+            ArrayList<String> listaMascotas = new ArrayList<>();
+
+            for (int i = 0; i<jsonMascotas.length();i++){
+                JSONObject jsonObject = jsonMascotas.getJSONObject(i);
+                listaMascotas.add(jsonObject.getString("tipo")+" "+ jsonObject.getString("nombre"));
+            }
+            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listaMascotas);
+            lstMascotas.setAdapter(adapter);
+        }catch (Exception e){
+            Log.e("ErrorJSON", e.toString());
+        }
     }
 }
--- app/src/main/java/com/example/appmascotas/Registrar.java
+++ app/src/main/java/com/example/appmascotas/Registrar.java
@@ -1,6 +1,6 @@
 package com.example.appmascotas;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,16 +12,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Registrar extends AppCompatActivity {
-    EditText edtTipo, edtNombre, edtColor, edtPeso;
-    Button btnRegistrarM;
+import com.android.volley.Network;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-    private void loadUI() {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Registrar extends AppCompatActivity {
+
+
+    String tipo , nombre , color;
+
+    double peso;
+    EditText edtTipo , edtNombre , edtColor , edtPeso;
+
+    Button btnRegistrarMascotas;
+
+    //Enviar /rciber los datos hacia el servicio
+    RequestQueue requestQueue;
+
+    //URL
+
+    private final String URL = "http://192.168.101.34:3000/mascotas/";
+    private void loadUI(){
         edtTipo = findViewById(R.id.edtTipo);
-        edtNombre = findViewById(R.id.edtNombre);
         edtColor = findViewById(R.id.edtColor);
-        edtPeso = findViewById(R.id.edtPeso);
-        btnRegistrarM = findViewById(R.id.btnRegistrarM);
+        edtNombre = findViewById(R.id.edtNombre);
+        edtPeso= findViewById(R.id.edtPeso);
+
+        btnRegistrarMascotas = findViewById(R.id.btnRegistrarM);
     }
 
     @Override
@@ -31,66 +56,149 @@ public class Registrar extends AppCompatActivity {
         setContentView(R.layout.activity_registrar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Metodo con referencias
         loadUI();
 
         //Eventos
-        btnRegistrarM.setOnClickListener((v)-> {registrarMascota();});
+        btnRegistrarMascotas.setOnClickListener(v -> {validarRegistro();});
 
     }
 
-    private void registrarMascota() {
-        if (edtTipo.getText().toString().isEmpty()) {
-            edtTipo.setError("Complete con Perro, Gato");
+
+    private void resetUI(){
+        edtTipo.setText(null);
+        edtNombre.setText(null);
+        edtColor.setText(null);
+        edtPeso.setText(null);
+        edtTipo.requestFocus();
+    }
+
+    private void validarRegistro(){
+
+        //CORE = NUCLEO
+        if(edtTipo.getText().toString().isEmpty()){
+            edtTipo.setError("Complete con Perro , Gato");
             edtTipo.requestFocus();
             return;
+
         }
-        if (edtNombre.getText().toString().isEmpty()) {
+
+        if (edtNombre.getText().toString().isEmpty()){
             edtNombre.setError("Escriba el nombre");
             edtNombre.requestFocus();
             return;
         }
-        if (edtColor.getText().toString().isEmpty()) {
-            edtColor.setError("Escriba el color");
+
+        if (edtColor.getText().toString().isEmpty()){
+            edtColor.setError("Este campo es obligatorio");
             edtColor.requestFocus();
             return;
         }
-        if (edtPeso.getText().toString().isEmpty()) {
-            edtPeso.setError("Escriba el peso");
+
+        if (edtPeso.getText().toString().isEmpty()){
+            edtPeso.setError("Ingrese un valor");
             edtPeso.requestFocus();
             return;
         }
 
-        String tipo = edtTipo.getText().toString().trim();
-        String nombre = edtTipo.getText().toString().trim();
-        String color = edtTipo.getText().toString().trim();
-        double peso = Double.parseDouble(edtPeso.getText().toString());
+        tipo = edtTipo.getText().toString().trim();
+        nombre = edtNombre.getText().toString().trim();
+        color = edtColor.getText().toString().trim();
+        peso = Double.parseDouble(edtPeso.getText().toString());
 
-        //Error sino es Perro o Gato
-        if(!tipo.equals("Perro") && !tipo.equals("Gato")){
-            edtTipo.setError("Solo se permite: Pero, Gato");
-            edtTipo.requestFocus();
+        //Error SINO es Perro o Gato
+        if (!tipo.equals("Perro") && !tipo.equals("Gato")){
+            edtTipo.setError("Solo se permite: Perros , Gato");
             return;
         }
 
-        if(peso < 0) {
-            edtPeso.setError("Solo se permiten valores positivos");
+        if (peso < 0){
+            edtPeso.setError(("Solo se premiten valores positivos"));
             edtPeso.requestFocus();
             return;
+
         }
 
-        //Solicitar la confirmacion
+        //Solicitar la confirmación
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Mascotas");
         builder.setMessage("¿Seguro de registrar?");
 
-        builder.setPositiveButton("Si",(a,b)-> {
-            Toast.makeText(getApplicationContext(),"Guardado",Toast.LENGTH_SHORT).show();
+        builder.setPositiveButton("Si",(a,b) ->{
+            registrarMascota();
         });
-        builder.setNegativeButton("No",null);
+
+        builder.setNegativeButton("No", null);
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    private void registrarMascota(){
+        //Comunicación
+        requestQueue = Volley.newRequestQueue(this);
+
+        //POST = requiere un JSON(datos a enviar)
+
+        JSONObject jsonObject = new JSONObject();
+
+        //Asignar los valores de las cajas
+
+        try{
+            jsonObject.put("tipo" ,tipo);
+            jsonObject.put("nombre", nombre);
+            jsonObject.put("color" , color);
+            jsonObject.put("pesokg" , peso);
+        }catch (JSONException e){
+            Log.e("Error" , e.toString());
+        }
+
+        Log.d("ValoresWS" , jsonObject.toString());
+
+        //Definir objeto (respuesta obtener)
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                URL,
+                jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        //EXITO
+                        Log.d("Resultado " , jsonObject.toString());
+
+                        //En el mensaje de confirmacion mostrar el ID  generado
+                        Toast.makeText(getApplicationContext() , "Guardado correctamente" , Toast.LENGTH_SHORT).show();
+                        resetUI();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        NetworkResponse response = volleyError.networkResponse;
+
+                        //evaluar por codigo de error
+                        if (response != null && response.data != null){
+                            //capturar el codigo de error 4xx , 5xx
+                            int statusCode = response.statusCode;
+                            String errorJson = new String(response.data);
+
+                            Log.e("VolleyError" , "Código: " + statusCode);
+                            Log.e("VolleyError" , "Cuerpo:" + errorJson);
+                        }else{
+                            Log.e("VolleyError" , "Sin respuesta de red");
+                        }
+                        //Log.e("Error WS" , volleyError.toString());
+                    }
+                }
+        );
+
+        //Ejecutamos el proceso
+        requestQueue.add(jsonObjectRequest);
     }
 }
