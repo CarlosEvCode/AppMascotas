c726dca1c5647a7e51a494c9129c6dcc0867ba56
CarlosMS <programer.cm12@gmail.com>
2026-03-19 12:57:18 -0500

    Creacion del layout de actualizar y validando q el id llega

--- app/src/main/java/com/example/appmascotas/Actualizar.java
+++ app/src/main/java/com/example/appmascotas/Actualizar.java
@@ -1,6 +1,8 @@
 package com.example.appmascotas;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,11 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Actualizar extends AppCompatActivity {
 
+    EditText resID, edtATipo, edtANombre, edtAColor, edtAPeso;
+    Button btnActualizar;
+
+    private void loadUI(){
+        resID = findViewById(R.id.resID);
+        edtATipo = findViewById(R.id.edtATipo);
+        edtANombre = findViewById(R.id.edtANombre);
+        edtAColor = findViewById(R.id.edtAColor);
+        edtAPeso = findViewById(R.id.edtAPeso);
+        btnActualizar = findViewById(R.id.btnActualizar);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         int idRecibido = getIntent().getIntExtra("idEnviado",0);
-        Toast.makeText(this,"Editar: "+idRecibido,Toast.LENGTH_SHORT).show();
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_actualizar);
@@ -23,5 +35,11 @@ public class Actualizar extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             return insets;
         });
+
+        loadUI();
+
+        if (idRecibido > 0) {
+            resID.setText(String.valueOf(idRecibido));
+        }
     }
 }
